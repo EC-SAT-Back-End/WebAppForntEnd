171ba78c9d91a1116620717072f2233974ce9d5a
Ahmed Break <Ahmed.Break@ec-sat.local>
2021-02-13 01:20:08 +0200

    add header-footer ng-bootstrab

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DefaultRoutingModule } from './default-routing.module';
+import { DefaultComponent } from './default.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [DefaultComponent],
   imports: [
     CommonModule,
     DefaultRoutingModule
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    NgbModule
+
+  ],
+  exports:[NgbModule],
+  entryComponents:[]
+})
+export class SharedModule { }
